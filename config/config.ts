abbdf25c2476471436ff1be019d30eec40c2bd98
ahKevinXy <2502329620@qq.com>
2023-07-07 21:55:53 +0800

    删除页面

--- config/config.ts
+++ config/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         },
         // dark: true,
     },
-    base: "/umax-admin/",
+    //base: "/umax-admin/",
     publicPath: "/umax-admin/",
     hash: true,
     access: {},
@@ -18,12 +18,7 @@ export default defineConfig({
     layout: {
         title: 'Umax Admin',
     },
-    headScripts: [
-        "https://gw.alipayobjects.com/os/lib/react/18.1.0/umd/react.production.min.js",
-        "https://gw.alipayobjects.com/os/lib/react-dom/18.1.0/umd/react-dom.production.min.js",
-        "https://lib.baomitu.com/axios/0.27.2/axios.min.js",
-        "https://g.alicdn.com/mylib/moment/2.24.0/min/moment.min.js",
-    ],
+
     routes: [
         {
             path: '/',
@@ -32,18 +27,17 @@ export default defineConfig({
         {
             name: '首页',
             path: '/home',
-            component: './Home',
+            component: '/Home',
         },
         {
             name: '权限演示',
             path: '/access',
-            target: '_blank',
-            component: './Access',
+            component: 'Access',
         },
         {
             name: '登录',
             path: '/login',
-            component: './Login',
+            component: 'Login',
             exact: true,
             layout: false,
 
@@ -51,22 +45,15 @@ export default defineConfig({
         {
             name: '银行',
             path: '/bank',
-            component: './Bank'
+            component: 'Bank'
         },
-        {
-            name: ' CRUD 示例',
-            path: '/table',
-            component: './Table',
-        },
+
 
     ],
     npmClient: 'yarn',
 
     analytics: {
         ga_v2: 'G-RXDN42SD0T', // google analytics 的 key (GA 4)
-
-
-
     }
 
 });
