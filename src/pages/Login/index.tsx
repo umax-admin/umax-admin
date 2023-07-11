24bf32768b053378eb1b57d7b8e2f22ce8b285f0
ahKevinXy <2502329620@qq.com>
2023-07-11 23:43:47 +0800

    修改提示

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -116,7 +116,7 @@ export default () => {
                                 size: 'large',
                                 prefix: <LockOutlined className={'prefixIcon'} />,
                             }}
-                            placeholder={'密码: ant.design'}
+                            placeholder={'密码: 123456789'}
                             rules={[
                                 {
                                     required: true,
